b44ec5a5cb0c1b58f74fc1453864e726024ae494
Derek Stavis <dekestavis@gmail.com>
2016-11-13 21:43:50 -0200

    Add Geolocation event component

---
+++ src/GeolocationExample.js
@@ -0,0 +1,68 @@
+import {GeolocationChange} from './react-event-components'
+import React, {Component} from 'react'
+
+export default class GeolocationChangeExample extends Component {
+  state = {}
+
+  handleLocationChange ({ coords, timestamp }) {
+    this.setState({
+      coords: {
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+        altitude: coords.altitude,
+        accuracy: coords.accuracy,
+        altitudeAccuracy: coords.altitudeAccuracy,
+        heading: coords.heading,
+        speed: coords.speed
+      },
+      timestamp
+    })
+  }
+
+  render() {
+    const {
+      coords = {},
+      timestamp = 0
+    } = this.state
+
+    const {
+      latitude,
+      longitude,
+      altitude,
+      heading,
+      speed,
+      accuracy,
+      altitudeAccuracy
+    } = coords
+
+    return (
+      <section>
+        <GeolocationChange do={this.handleLocationChange.bind(this)} />
+        <h2>Geolocation</h2>
+        {
+          this.state.coords ?
+            <div>
+              <img alt="location map" src={`https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&zoom=16&size=300x300&sensor=false`} />
+              <ul>
+                <li>Updated at: { new Date(timestamp).toString() }</li>
+                <li>Latitude: { latitude }</li>
+                <li>Longitude: { longitude }</li>
+                <li>Altitude: { altitude }</li>
+                <li>Heading: { heading }</li>
+                <li>Speed: { speed }</li>
+                <li>
+                  Accuracy:
+                  <ul>
+                    <li>Latitude/Longitude: { accuracy }</li>
+                    <li>Altitude: { altitudeAccuracy }</li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          :
+            <span>Waiting for location...</span>
+        }
+      </section>
+    )
+  }
+}
---
+++ src/react-event-components/GeolocationChange.js
@@ -0,0 +1,47 @@
+const { Component, PropTypes } = require('react')
+
+const geolocation = 'geolocation' in navigator ? navigator.geolocation : null
+
+class GeolocationChange extends Component {
+  componentDidMount() {
+    if (geolocation) {
+      const watchId = geolocation.watchPosition(this.props.do, this.handleError)
+      this.setState({ watchId })
+    }
+  }
+
+  componentWillUnmount() {
+    if (geolocation) {
+      geolocation.clearWatch(this.props.watchId)
+    }
+  }
+
+  handleError (error) {
+    if (typeof this.props.onError === 'function') {
+      this.props.onError(error)
+    }
+  }
+
+  shouldComponentUpdate() {
+    return false
+  }
+
+  render() {
+    return null
+  }
+}
+
+GeolocationChange.propTypes = {
+  /**
+   * Triggered when the location change 
+   * @type {Function}
+   */
+  do: PropTypes.func.isRequired,
+  /**
+   * Triggered when location error occurs
+   * @type {Function}
+   */
+  onError: PropTypes.func
+}
+
+module.exports = GeolocationChange
