9a343782019c043933514ba102b615ccceefe9af
Felquis Gimenes <frgformenton@gmail.com>
2016-11-13 21:07:01 -0200

    Fix syntax error caused by comma

--- src/EveryExample.test.js
+++ src/EveryExample.test.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom'
 import EveryExample from './EveryExample'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div'),
+  const div = document.createElement('div')
   ReactDOM.render(<EveryExample />, div)
 })
--- src/KeyDownExample.test.js
+++ src/KeyDownExample.test.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom'
 import KeyDownExample from './KeyDownExample'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div'),
+  const div = document.createElement('div')
   ReactDOM.render(<KeyDownExample />, div)
 })
