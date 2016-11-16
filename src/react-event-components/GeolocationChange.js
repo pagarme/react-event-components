bb762f8e182ea084e973b20e1ee57a5143d992f8
Derek Stavis <dekestavis@gmail.com>
2016-11-16 14:27:31 -0200

    Fix state/props and binding shit

--- src/react-event-components/GeolocationChange.js
+++ src/react-event-components/GeolocationChange.js
@@ -5,14 +5,17 @@ const geolocation = 'geolocation' in navigator ? navigator.geolocation : null
 class GeolocationChange extends Component {
   componentDidMount() {
     if (geolocation) {
-      const watchId = geolocation.watchPosition(this.props.do, this.handleError)
+      const watchId = geolocation.watchPosition(
+        this.props.do,
+        this.handleError.bind(this)
+      )
       this.setState({ watchId })
     }
   }
 
   componentWillUnmount() {
     if (geolocation) {
-      geolocation.clearWatch(this.props.watchId)
+      geolocation.clearWatch(this.state.watchId)
     }
   }
 
