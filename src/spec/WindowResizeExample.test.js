af01acf7217de4283d5494d7fd358891c6794fd2
Felquis Gimenes <frgformenton@gmail.com>
2016-12-15 01:47:01 -0200

    Rebase + Fix test

--- src/spec/WindowResizeExample.test.js
+++ src/spec/WindowResizeExample.test.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import WindowResizeExample from './WindowResizeExample'
+import WindowResizeExample from '../WindowResizeExample'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div'),
+  const div = document.createElement('div')
   ReactDOM.render(<WindowResizeExample />, div)
 })
