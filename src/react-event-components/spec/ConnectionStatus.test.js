c8d1be865f886ca5ca72c111229e42d9399178f2
Victor Igor <victor.0w3@hotmail.com>
2017-08-29 17:09:47 -0300

    indent style fixed

--- src/react-event-components/spec/ConnectionStatus.test.js
+++ src/react-event-components/spec/ConnectionStatus.test.js
@@ -6,7 +6,7 @@ it('renders without crashing', () => {
   const div = document.createElement('div')
   const handle = () => {}
   ReactDOM.render(<div>
-                    <ConnectionStatus when='online' do={handle} />
-                    <ConnectionStatus do={handle} />
-                  </div>, div)
+      <ConnectionStatus when='online' do={handle} />
+      <ConnectionStatus do={handle} />
+    </div>, div)
 })
